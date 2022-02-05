d6f681ce0ca8aa20667cd439744c5d044f7feb8e
Naveen <nj>
2022-02-05 22:22:23 +0530

    fix main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import '@vue/ui/dist/vue-ui.css'
+// import '@vue/ui/dist/vue-ui.css'
 import App from './App.vue'
-import VueUi from '@vue/ui'
+// import VueUi from '@vue/ui'
 import './index.css'
 
-createApp(App).use(VueUi).mount('#app')
+createApp(App).mount('#app')
